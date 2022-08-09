c20a37142fca42adde0bc8f97f8b7459e515b5a9
markxkamau <markxkamau@gmail.com>
2022-08-09 23:18:09 +0300

    Main class generated for the spring application

---
+++ src/main/java/com/example/random_generator/paricipant_selection/ParicipantSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.random_generator.paricipant_selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParicipantSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParicipantSelectionApplication.class, args);
+	}
+
+}
