a75b2618fdaf0bb352b4a7008ebf2ecf0a7c97d9
markxkamau <markxkamau@gmail.com>
2022-08-09 23:18:20 +0300

    Main class generated for the spring application

---
+++ src/test/java/com/example/random_generator/paricipant_selection/ParicipantSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.random_generator.paricipant_selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParicipantSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
